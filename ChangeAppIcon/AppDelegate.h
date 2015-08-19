5ec6e1f932ba1f4cf81e1c64f07752298c6d79ca
liusilan <413769707@qq.com>
2015-08-19 11:20:48 +0800

    changeIcon

---
+++ ChangeAppIcon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChangeAppIcon
+//
+//  Created by liusilan on 15/8/18.
+//  Copyright (c) 2015年 YY, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChangeAppIcon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChangeAppIcon
+//
+//  Created by liusilan on 15/8/18.
+//  Copyright (c) 2015年 YY, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
